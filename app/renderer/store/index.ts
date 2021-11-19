fcca645b27be7aaee7de8ab723308f265de440a1
Kungg <1592500694@qq.com>
2021-11-19 15:00:50 +0800

    chore: 增加通用组件和初始数据

--- app/renderer/store/index.ts
+++ app/renderer/store/index.ts
@@ -4,8 +4,9 @@ import logger from 'redux-logger';
 import RcReduxModel from 'rc-redux-model';
 
 import globalModel from './globalModel';
+import resumeModel from './resumeModel';
 
-const reduxModel = new RcReduxModel([globalModel])
+const reduxModel = new RcReduxModel([globalModel,resumeModel])
 
 const reducerList = combineReducers(reduxModel.reducers)
 

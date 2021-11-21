46bd05d47eca16127e37154ce88087d0be491ccc
Kungg <1592500694@qq.com>
2021-11-21 16:26:07 +0800

    feat: 完成对所以简单数据的录入

--- app/renderer/container/resume/ResumeContent/UseForm/Personal/index.tsx
+++ app/renderer/container/resume/ResumeContent/UseForm/Personal/index.tsx
@@ -1,3 +1,5 @@
+
+   
 /**
  * @description 个人信息Form
  */
@@ -7,6 +9,7 @@ import MyModal from '@common/components/MyModal';
 import MyInput from '@common/components/MyInput';
 import { useSelector } from 'react-redux';
 import useUpdateResumeHook from '@src/container/resume/ResumeContent/useUpdateResumeHook';
+
 interface IProps {
   onClose: () => void;
 }
@@ -30,9 +33,14 @@ function Personal({ onClose }: IProps) {
             <span styleName="require">*</span>姓 名 ：
           </div>
           <div styleName="right">
-            <MyInput onChange={(e) => {
-              updateResumeHook('base/username',e.target?.value||'')
-            }} value={base?.username || ''} placeholder="请输入姓名" allowClear={true} />
+            <MyInput
+              onChange={(e) => {
+                updateResumeHook('base/username', e.target?.value || '');
+              }}
+              value={base?.username || ''}
+              placeholder="请输入姓名"
+              allowClear={true}
+            />
           </div>
         </div>
         <div styleName="flex">
@@ -40,9 +48,14 @@ function Personal({ onClose }: IProps) {
             <span styleName="require">*</span>籍 贯 ：
           </div>
           <div styleName="right">
-            <MyInput onChange={(e) => {
-              //  updateResumeHook('base/hometown', e.target?.value || '');
-            }} value={base?.hometown || ''} placeholder="请输入籍贯" allowClear={true} />
+            <MyInput
+              onChange={(e) => {
+                updateResumeHook('base/hometown', e.target?.value || '');
+              }}
+              value={base?.hometown || ''}
+              placeholder="请输入籍贯"
+              allowClear={true}
+            />
           </div>
         </div>
         <div styleName="flex">

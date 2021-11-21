90d8f12617429d7917358f7ee31c8cd5042b9792
Kungg <1592500694@qq.com>
2021-11-21 16:43:25 +0800

    feat: 简历连接redux数据

--- app/renderer/container/resume/ResumeContent/UseTemplate/templateOne/components/Avatar/index.tsx
+++ app/renderer/container/resume/ResumeContent/UseTemplate/templateOne/components/Avatar/index.tsx
@@ -7,42 +7,41 @@ import './index.less';
 import { useSelector } from 'react-redux';
 import uploadIcon from '@assets/icon/upload.png';
 import MyButton from '@common/components/MyButton';
-import useUpdataResumeHook from '../../../../useUpdateResumeHook'
-import ImageUpload from '@src/common/components/MyUpload/ImageUpload';
+import ImageUpload from '@common/components/MyUpload/ImageUpload';
+import useUpdateResumeHook from '@src/container/resume/ResumeContent/useUpdateResumeHook';
 
 function Avatar() {
-  const updateResumeHook = useUpdataResumeHook();
   const base: TSResume.Base = useSelector((state: any) => state.resumeModel.base);
-  const onUpdateUserAvatar =(avatarUrl:string) =>{
-    updateResumeHook<string>('base/avatar',avatarUrl)
-  }
+  const updateResumeHook = useUpdateResumeHook();
+
+  const onUpdateUserAvatar = (avatarUrl: string) => {
+    updateResumeHook<string>('base/avatar', avatarUrl);
+  };
+
   return (
     <div styleName="box">
-      {!base?.avatar&&(
-         <ImageUpload
-         icon={uploadIcon}
-         accept="image/*"
-         multiple={false}
-         onAfterChange={(files: TSUpload.File[]) => {
-           onUpdateUserAvatar(files[0]?.base64URL);
-         }}
-       />
-      )
-      }{
-        base?.avatar&&(
-          <div></div>
-        )
-      }
-      <div styleName="avatar">
-        <img src={base?.avatar} />
-        <div styleName="mask">
+      {!base?.avatar && (
+        <ImageUpload
+          icon={uploadIcon}
+          accept="image/*"
+          multiple={false}
+          onAfterChange={(files: TSUpload.File[]) => {
+            onUpdateUserAvatar(files[0]?.base64URL);
+          }}
+        />
+      )}
+      {base?.avatar && (
+        <div styleName="avatar">
+          <img src={base?.avatar} />
+          <div styleName="mask">
             <MyButton size="small" className="btn-change" onClick={() => onUpdateUserAvatar('')}>
               更换
             </MyButton>
           </div>
-      </div>
+        </div>
+      )}
     </div>
   );
 }
 
-export default Avatar
+export default Avatar;
--- app/renderer/container/resume/ResumeContent/UseTemplate/templateOne/components/BaseInfo/index.tsx
+++ app/renderer/container/resume/ResumeContent/UseTemplate/templateOne/components/BaseInfo/index.tsx
@@ -2,22 +2,30 @@
  * @desc 基本信息
  * @author pengdaokuan
  */
-import React from 'react';
-import '../../../styles/template-one.less';
-
-function BaseInfo() {
-  return (
-    <div styleName="container">
-      <p styleName="title">基本信息 Basic</p>
-      <ul styleName="content">
-        <li>院校：湖南瞎说大学</li>
-        <li>专业：软件工程</li>
-        <li>学历：本科</li>
-        <li>学年：2015.09 - 2019.06</li>
-        <li>籍贯：海南·海口</li>
-      </ul>
-    </div>
-  );
-}
+ import React from 'react';
+ import '../../../styles/template-one.less';
+ import { useSelector } from 'react-redux';
+ 
+ function BaseInfo() {
+   const base: TSResume.Base = useSelector((state: any) => state.resumeModel.base);
+ 
+   return (
+     <div styleName="container">
+       <p styleName="title">基本信息 Basic</p>
+       <ul styleName="content">
+         {base?.school && <li>院校：{base?.school}</li>}
+         {base?.major && <li>专业：{base?.major}</li>}
+         {base?.degree && <li>学历：{base?.degree}</li>}
+         {base?.onSchoolTime && base?.onSchoolTime?.beginTime && base?.onSchoolTime?.endTime && (
+           <li>
+             学年：{base?.onSchoolTime?.beginTime} - {base?.onSchoolTime?.endTime}
+           </li>
+         )}
+         {base?.hometown && <li>籍贯：{base?.hometown}</li>}
+       </ul>
+     </div>
+   );
+ }
+ 
 
 export default BaseInfo;
